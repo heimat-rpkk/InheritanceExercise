4370b0bfc35246b46838629e81e24dee9f8476d8
heimat-rpkk <111064412+heimat-rpkk@users.noreply.github.com>
2026-09-10 20:42:48 +0300

    character.gd updated

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -4,8 +4,7 @@ extends CharacterBody2D
 @export var speed : float = 200.0
 @export var gravity : float = 900.0
 @export var health : int = 3
-# Tämä lista kertoo mihin törmättiin edellisessä framessa
-var already_collided: Array[Node] = []
+@export var is_player : bool = false
 var can_take_damage := true
 
 
@@ -40,7 +39,7 @@ func take_damage(amount: int):
 func start_invincibility():
 	can_take_damage = false
 	# Luodaan ajastin koodilla
-	await get_tree().create_timer(0.5).timeout 
+	await get_tree().create_timer(1.5).timeout 
 	can_take_damage = true
 
 
@@ -55,8 +54,13 @@ func check_collisions() -> void:
 		var collider = collision.get_collider()
 		
 		if collider is Character:
-			if collider.has_method("take_damage"):
+			# Pelaaja hyppää vihollisen päälle
+			if is_player and collision.get_normal().y < -0.7:
+				collider.take_damage(1)
+				velocity.y = -300
+
+			# Tavallinen törmäys
+			else:
 				collider.take_damage(1)
 			
-				# Oma hahmo pomppaa hieman ylöspäin
-				velocity.y = -200
+			
